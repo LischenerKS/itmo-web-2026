c7cfc8932eb2503fcb522e8a9d61d96c41f26abc
lischenerks <lischenerks@gmail.com>
2026-09-18 17:15:40 +0300

    feat: add arrows, ticks, and labels to axes

--- script.js
+++ script.js
@@ -31,11 +31,60 @@ document.addEventListener('DOMContentLoaded', () => {
     context.strokeStyle = 'black';
     context.beginPath();
 
+    //x
     context.moveTo(0, centerY);
     context.lineTo(canvas.width, centerY);
 
+    context.moveTo(canvas.width - 10, centerY - 5);
+    context.lineTo(canvas.width, centerY);
+    context.lineTo(canvas.width - 10, centerY + 5);
+
+    //y
     context.moveTo(centerX, 0);
     context.lineTo(centerX, canvas.height);
 
+    context.moveTo(centerX - 5, 10);
+    context.lineTo(centerX, 0);
+    context.lineTo(centerX + 5, 10);
+
+    context.fillStyle = 'black';
+    context.font = "12px Arial";
+
+    context.fillText("x", canvas.width - 15, centerY - 15);
+    context.fillText("y", centerX + 15, 15);
+
     context.stroke();
+
+
+
+
+    const tick = 5;
+
+
+
+    const points = [
+        { multi: 1, label: "R" },
+        { multi: 0.5, label: "R/2" },
+        { multi: -0.5, label: "-R/2" },
+        { multi: -1, label: "-R" }
+    ];
+
+
+    points.forEach(p => {
+        const offset = p.multi * R;
+
+        //x
+        context.beginPath();
+        context.moveTo(centerX + offset, centerY - tick);
+        context.lineTo(centerX + offset, centerY + tick);
+        context.stroke();
+        context.fillText(p.label, centerX + offset - 10, centerY - 15);
+
+        //y
+        context.beginPath();
+        context.moveTo(centerX - tick, centerY - offset);
+        context.lineTo(centerX + tick, centerY - offset);
+        context.stroke();
+        context.fillText(p.label, centerX + 10, centerY - offset + 4);
+    });
 });
